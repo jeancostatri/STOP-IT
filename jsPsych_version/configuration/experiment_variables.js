0b387c6ad09ea15a1550efa84625b9e376d15eb3
jeancostatri <100489968+jeancostatri@users.noreply.github.com>
2022-07-14 09:26:59 -0700

    Update experiment_variables.js

--- jsPsych_version/configuration/experiment_variables.js
+++ jsPsych_version/configuration/experiment_variables.js
@@ -51,8 +51,7 @@ var NdesignReps_exp = 8;
 // Number of experimental blocks (excluding the first practice block).
 // Note that NexpBl = 0 will still run the practice block
 
-//var NexpBL = 4;
-var NexpBL = 1;
+var NexpBL = 4;
 
 
 // ----- CUSTOMISE THE TIME INTERVALS (in milliseconds)-----
